bf2094600828a89710adbc42a20f62eff8011bac
Russell Yanofsky <russ@yanofsky.org>
2014-09-08 18:51:54 -0400

    move remove-preserving-date script

--- src/remove-preserving-date_test.sh
+++ src/remove-preserving-date_test.sh
@@ -5,7 +5,7 @@ testEquality() {
   mkdir -p "$D/dst" "$D/src/1/2/3"
   touch "$D/src/1/2/3/a" "$D/src/1/2/3/b"
   find "$D" -print0 | xargs -0 touch -d"@0"
-  (cd "$D/src"; D="$D/dst" arh 1/2/3/a)
+  (cd "$D/src"; D="$D/dst" remove-preserving-date.sh 1/2/3/a)
   CH=$(rsync -nia --delete "$D/dst/" "$D/src/")
   assertEquals "$CH" "*deleting   1/2/3/b"$'\n'">f+++++++++ 1/2/3/a"
   rm -rf "$D"
