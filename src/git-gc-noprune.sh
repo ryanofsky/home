a0084e2744adb67c72f9552dc6c56cff1f66c7a9
Russell Yanofsky <russ@yanofsky.org>
2015-01-26 19:42:43 -0500

    fix path bug in git-gc-noprune.sh
    i think i had to make same fix earlier in work repo. didnt check

--- src/git-gc-noprune.sh
+++ src/git-gc-noprune.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # from http://stackoverflow.com/questions/12339165/how-to-pack-all-loose-objects-in-git
+set -e
+set -x
 git gc --no-prune
 git prune-packed
-find .git/objects/?? -type f | perl -pe 's@^\.git/objects/(..)/@$1@' | git pack-objects objects/pack/pack
+find .git/objects/?? -type f | perl -pe 's@^\.git/objects/(..)/@$1@' | git pack-objects .git/objects/pack/pack
 git prune-packed
