4f5dac33ce6d1c125f35ff586347442395064eed
Russell Yanofsky <russ@yanofsky.org>
2016-03-28 13:51:28 -0400

    add working torrent json import code

---
+++ src/2016/torrent/import.sh
@@ -0,0 +1,87 @@
+#!/bin/bash
+
+set -x
+set -e
+
+cd $(dirname $0)
+
+TMP_DIR=/mnt/fort/_tmp/torrent
+STATES_DIR=$TMP_DIR/states
+SORT_FILE=$TMP_DIR/states-sort
+JSON_DIR=$TMP_DIR/json
+SYMLINK_DIR=$TMP_DIR/symlinks
+
+# List input directories. Download dir is one directory that is
+# input/output.
+DOWNLOAD_DIR=/mnt/fort/_download
+DOWNLOAD_DIR=$TMP_DIR/_download # For testing only
+STATES=(
+old.3/latest/old/2009-07-26-backup/root/home/russ/.azureus/torrents
+old.47/latest/old/2012-01-28-rescue-toaster-root/home/russ/.azureus/torrents
+old.5/latest/old/2011-10-30-russ/.azureus/torrents
+old.9/latest/old/2012-02-15-russ/.azureus/torrents
+old.34/latest/old/2011-07-17-rabbit-root/home/russ/.config/deluge/state
+old.12/latest/old/2012-02-15-rabbit-russ/.config/deluge/state
+old.5/latest/old/2011-10-30-russ/.config/deluge/state
+old.9/latest/old/2012-02-15-russ/.config/deluge/state
+old.47/latest/old/2012-01-28-rescue-toaster-root/home/russ/.config/deluge/state
+old.58/latest/old/2014-05-21-homes/russ/.config/deluge/state
+old.58/latest/old/2014-05-21-homes/pia/.config/deluge/state
+$DOWNLOAD_DIR/pia-config-deluge/*/state
+$DOWNLOAD_DIR/pia2-config-deluge/*/state
+)
+DATA_DIRS=(
+$DOWNLOAD_DIR
+/mnt/fort/.snapshots/share@20140527T001746Z
+/mnt/fort/_tmp/data/old.56/latest/old/2014-03-25-share
+/mnt/fort/_tmp/data/old.57/latest/old/2014-03-25-share
+)
+
+# Set up temporary directory with an embedded mutable copy of
+# _download directory for testing.
+test ! -e $TMP_DIR/_download || btrfs su delete $TMP_DIR/_download
+test ! -e $TMP_DIR || btrfs su delete $TMP_DIR
+btrfs su create $TMP_DIR
+btrfs su snapshot /mnt/fort/_download $TMP_DIR/_download
+
+# Figure out ordering for state dirs.
+rm -rf $STATES_DIR $SORT_FILE
+for STATE in "${STATES[@]}"; do
+  if [ -z "${STATE%old.*}" ]; then
+     BUP_DIR=/mnt/fort/data bup restore $STATE/. -C $STATES_DIR/$STATE
+     STATE_DIR=$STATES_DIR/$STATE
+  else
+     STATE_DIR=$STATE
+  fi
+  MTIME=$(find $STATE_DIR -type f -printf '%T@ %p\n' | sort | tail -n1 | cut -f1 -d' ')
+  echo "$MTIME $STATE_DIR $STATE" >> $SORT_FILE
+done
+
+# Process state dirs.
+rm -rf $JSON_DIR
+mkdir $JSON_DIR
+(cd $JSON_DIR; git init)
+while read MTIME STATE_DIR STATE; do
+  python -c "import torrent; torrent.tojson('$STATE_DIR', '$JSON_DIR')"
+  (cd $JSON_DIR; git add .; git commit --allow-empty -m "Import torrents from $STATE")
+done < <(sort -n $SORT_FILE)
+echo "Check overridden states with (cd $JSON_DIR; git log -p -U0 --diff-filter=M)"
+
+# Create torrent dir with symlinks to data files.
+rm -rf $DOWNLOAD_DIR/torrent
+mkdir $DOWNLOAD_DIR/torrent
+python -c "import torrent; torrent.find_files('$JSON_DIR', '$DOWNLOAD_DIR/torrent', '${DATA_DIRS[*]}'.split())"
+
+# Create other symlink directories.
+rm -rf "$SYMLINK_DIR"
+for DATA_DIR in "${DATA_DIRS[@]}"; do
+  if [ "$DATA_DIR" != "$DOWNLOAD_DIR" ]; then
+     python -c "import torrent; torrent.make_symlink_tree('$DATA_DIR', '$SYMLINK_DIR$DATA_DIR')"
+  fi
+done
+
+# Add checksums.
+python -c "import torrent; torrent.load_sums('$JSON_DIR', '$DOWNLOAD_DIR/torrent', '/mnt/fort/_tmp/torrent.sums')"
+python -c "import torrent; torrent.compute_sums('$JSON_DIR', '$DOWNLOAD_DIR/torrent')"
+(cd $JSON_DIR; git add .; git commit --allow-empty -m "Run compute_sums to get md5 checksums.")
+#python -c "import torrent; torrent.dump_sums('$JSON_DIR', '$DOWNLOAD_DIR/torrent', '/mnt/fort/_tmp/torrent.sums')"
---
+++ src/2016/torrent/torrent.py
@@ -0,0 +1,507 @@
+#!/usr/bin/python2
+
+import hashlib
+import json
+import os
+import pickle
+import re
+import subprocess
+import sys
+
+from cStringIO import StringIO
+from collections import OrderedDict
+from deluge import bencode
+from deluge.core.torrentmanager import TorrentState, TorrentManagerState
+
+
+def tojson(in_dir, out_dir):
+    torrents = load_torrents(out_dir)
+
+    for f in os.listdir(in_dir):
+        if not f.endswith(".torrent"):
+            continue
+        torrent_path = os.path.join(in_dir, f)
+        data = bencode.bdecode(open(torrent_path).read())
+        torrent_id = hashlib.sha1(bencode.bencode(data["info"])).hexdigest()
+        torrent_name = f[:-len(".torrent")]
+        if re.match("^[0-9a-f]{40}$", torrent_name):
+            check(torrent_name == torrent_id, torrent_name)
+        t = torrents.setdefault(torrent_id, OrderedDict())
+        t["data"] = data
+        t.setdefault("download", OrderedDict())["mtime"] = os.path.getmtime(torrent_path)
+
+    state_file = os.path.join(in_dir, "torrents.state")
+    if os.path.exists(state_file):
+        state = pickle.load(open(state_file))
+        check(set(obj_dict(state).keys()) == {"torrents"})
+        default_torrent = TorrentState()
+        for st in state.torrents:
+            torrent_id = st.torrent_id
+            t = torrents.setdefault(torrent_id, OrderedDict())
+            t["state"]= tstate = sort_dicts(obj_dict(st))
+            for k, v in tstate.items():
+                if v == getattr(default_torrent, k):
+                    del tstate[k]
+
+    resume_file = os.path.join(in_dir, "torrents.fastresume")
+    if os.path.exists(resume_file):
+        resume = bencode.bdecode(open(resume_file).read())
+        for torrent_id, rt in resume.iteritems():
+            t = torrents.setdefault(torrent_id, OrderedDict())
+            t["fastresume"] = bencode.bdecode(rt)
+
+    # Sort dictionary keys
+    for torrent_id, t in torrents.iteritems():
+        sort_keys(t, ("data", "state", "fastresume", "download"))
+
+    save_torrents(torrents, out_dir)
+
+
+def fromjson(in_dir, out_dir):
+    torrents = load_torrents(in_dir)
+
+    os.mkdir(out_dir)
+    tm = TorrentManagerState()
+    fr = OrderedDict()
+    for torrent_id, t in sorted(torrents.items()):
+        for k, v in t.iteritems():
+            if k == "data":
+                with open(os.path.join(out_dir, torrent_id + ".torrent"),
+                          "w") as fp:
+                    fp.write(bencode.bencode(v))
+            elif k == "state":
+                tm.torrents.append(TorrentState(**v))
+            else:
+                check(k== "fastresume")
+                fr[torrent_id] = v
+
+    with open(os.path.join(out_dir, "torrents.state"), "w") as fp:
+        pickle.dump(tm, fp)
+
+    with open(os.path.join(out_dir, "torrents.fastresume"), "w") as fp:
+        fp.write(bencode.bencode(fr))
+
+
+def find_files(json_dir, torrent_dir, src_dirs):
+    torrents = load_torrents(json_dir)
+    base_idx = {}  # File basename -> torrent_id, file name
+    for torrent_id, torrent in torrents.items():
+        if not "data" in torrent:
+            continue
+        info = torrent["data"]["info"]
+        for path, length in get_torrent_files(info):
+            basename = os.path.basename(path)
+            if basename not in base_idx:
+                base_idx[basename] = torrent_id, path
+            elif base_idx[basename] not in (None, (torrent_id, path)):
+                print("Ignoring non-unique basename {!r} in {} and {}".format(basename, (torrent_id, path), base_idx[basename]))
+                base_idx[basename] = None
+
+    # Crawl source directory and make matches
+    file_idx = {}  # torrent_id, file number -> source file
+    subs = set()  # Set of torrent -> source path substiutions.
+    for src_dir in src_dirs:
+        for root, dirs, files in os.walk(src_dir):
+            if root == torrent_dir:
+                del dirs[:]
+                continue
+            rel = os.path.relpath(root, src_dir)
+            for f in files:
+                file_info = base_idx.get(f)
+                if file_info:
+                    src_path = os.path.join(root, f)
+                    file_idx.setdefault(file_info, src_path)
+                    torrent_id, torrent_path = file_info
+                    src_parts = src_path.split(os.sep)
+                    torrent_parts = torrent_path.split(os.sep)
+                    while src_parts and torrent_parts and src_parts[-1] == torrent_parts[-1]:
+                        src_parts.pop()
+                        torrent_parts.pop()
+                    subs.add((os.sep.join(torrent_parts + [""]), os.sep.join(src_parts + [""])))
+
+    subs = sorted(subs)
+
+    # Create torrent dirs inferring file locations with file_idx and subs.
+    for torrent_id, torrent in torrents.items():
+        if not "data" in torrent:
+            continue
+        info = torrent["data"]["info"]
+
+        timestamp = field(torrent, "state", "time_added") or field(torrent, "fastresume", "added_time") or field(torrent, "download", "mtime")
+        touch_paths = set()
+
+        for path, length in get_torrent_files(info):
+            # Look for direct match when file has unique basename.
+            src_path = file_idx.get((torrent_id, path))
+
+            # Look for indirect match based on direct match locations.
+            if not src_path:
+                for old, new in subs:
+                    if not old:
+                        new_path = new + path
+                    elif path.startswith(old):
+                        new_path = path.replace(old, new, 1)
+                    else:
+                        continue
+                    if os.path.exists(new_path):
+                        src_path = new_path
+                        break
+
+                    # Look for paths where vuze replaced unicode characters with ?.
+                    try:
+                        utf8_new_path = new_path.decode("utf8")
+                    except UnicodeDecodeError:
+                        pass
+                    else:
+                        ascii_new_path = utf8_new_path.encode("ascii", errors="replace")
+                        if ascii_new_path != new_path and os.path.exists(ascii_new_path):
+                            src_path = ascii_new_path
+                            break
+
+            if not src_path:
+                src_path = os.devnull
+
+            # Create parent dirs and save paths for later call to touch.
+            torrent_dirname = os.path.dirname(os.path.join(torrent_id, path))
+            torrent_dir_parts = torrent_dirname.split(os.sep)
+            torrent_dir_parents = [os.path.join(torrent_dir, *torrent_dir_parts[:i+1])
+                                   for i in range(len(torrent_dir_parts))]
+            for torrent_dir_parent in torrent_dir_parents:
+                touch_paths.add(torrent_dir_parent)
+                if not os.path.exists(torrent_dir_parent):
+                    os.mkdir(torrent_dir_parent)
+
+            # Create symlink.
+            torrent_path = os.path.join(torrent_dir, torrent_id, path)
+            if os.path.islink(torrent_path):
+                old_src_path = os.readlink(torrent_path)
+                if old_src_path == src_path:
+                    continue
+                check(old_src_path, os.devnull)
+                os.unlink(torrent_path)
+            elif os.path.exists(torrent_path):
+                continue
+            os.symlink(src_path, torrent_path)
+            touch_paths.add(torrent_path)
+
+        subprocess.check_call(["touch", "-hd@{}".format(timestamp)]
+                              + list(touch_paths))
+
+
+def compute_sums(json_dir, torrent_dir):
+    for torrent_id in list_json_torrents(json_dir):
+        torrent = load_json_torrent(json_dir, torrent_id)
+        make_str(torrent)
+
+        if not "data" in torrent:
+            continue
+
+        print("== {} ==".format(torrent_id))
+        info = torrent["data"]["info"]
+        download = torrent.setdefault("download", OrderedDict())
+        md5s = download.setdefault("md5", [])
+        if not md5s:
+            for _ in get_torrent_files(info):
+                md5s.append("")
+
+        total_length = sum(length for name, length in get_torrent_files(info))
+        total_bytes = 0
+        piece_length = info["piece length"]
+        piece_bytes = 0
+        shas = StringIO(info["pieces"])
+        check(((total_length + piece_length - 1) // piece_length)*20 == len(shas.getvalue()))
+        sha1 = hashlib.sha1()
+
+        def skip_pieces(offset):
+            for _ in range((piece_bytes + offset) // piece_length):
+                sha = shas.read(20)
+                check(len(sha) == 20, repr(sha))
+            new_piece_bytes = (piece_bytes + offset) % piece_length
+            if new_piece_bytes and total_bytes == total_length:
+                sha = shas.read(20)
+                check(len(sha) == 20, repr(sha))
+                new_piece_bytes = 0
+            new_sha1 = hashlib.sha1() if new_piece_bytes == 0 else None
+            return new_piece_bytes, new_sha1
+
+        for i, (rel_path, file_length) in enumerate(get_torrent_files(info)):
+            abs_path = os.path.join(torrent_dir, torrent_id, rel_path)
+            size = os.path.getsize(abs_path)
+            skip_file = False
+            if size != file_length:
+                print >> sys.stderr, ("Bad file size {!r}, expected {} bytes, found {}.".format(rel_path, file_length, size))
+                skip_file = True
+            elif md5s[i]:
+                skip_file = True
+
+            if skip_file:
+                #print("Skip {!r} {}".format(rel_path, file_length))
+                total_bytes += file_length
+                piece_bytes, sha1 = skip_pieces(file_length)
+                continue
+
+            with open(abs_path, "rb") as fp:
+                file_bytes = 0
+                md5 = hashlib.md5()
+
+                while True:
+                    piece = fp.read(piece_length - piece_bytes)
+                    if not piece:
+                        assert file_bytes == file_length
+                        break
+                    piece_bytes += len(piece)
+                    total_bytes += len(piece)
+                    file_bytes += len(piece)
+                    if sha1:
+                      sha1.update(piece)
+                    md5.update(piece)
+                    if piece_bytes == piece_length or total_bytes == total_length:
+                        piece_bytes = 0
+                        sha = shas.read(20)
+                        check(len(sha) == 20, repr(sha))
+                        if sha1:
+                            sha1_digest = sha1.digest()
+                            if sha != sha1_digest:
+                                print >> sys.stderr, "Bad checksum {!r} pos {}".format(rel_path, file_bytes - len(piece))
+                                total_bytes += file_length - file_bytes
+                                piece_bytes, sha1 = skip_pieces(file_length - file_bytes)
+                                md5 = None
+                                break
+                        sha1 = hashlib.sha1()
+            if md5:
+                md5_hexdigest = md5.hexdigest()
+                #print >> sys.stderr, ("Good checksum {!r} md5 {}".format(rel_path, md5_hexdigest))
+                md5s[i] = md5_hexdigest
+
+        check(piece_bytes == 0)
+        check(total_bytes == total_length)
+        r = shas.read()
+        check(len(r) == 0, len(r))
+
+        make_unicode(torrent)
+        save_json_torrent(torrent, json_dir, torrent_id)
+
+
+def dump_sums(json_dir, torrent_dir, sum_file):
+    torrents = load_torrents(json_dir)
+    sums = []
+
+    for torrent_id, torrent in torrents.items():
+        if not "data" in torrent:
+            continue
+        info = torrent["data"]["info"]
+
+        if not "download" in torrent:
+            continue
+        md5s = torrent["download"]["md5"]
+
+        for md5, (rel_path, file_length) in zip(md5s, get_torrent_files(info)):
+            src_path = os.path.join(torrent_dir, torrent_id, rel_path)
+            if os.path.islink(src_path):
+                src_path = os.readlink(src_path)
+            if md5:
+                sums.append([src_path, md5])
+
+    sums.sort()
+    make_unicode(sums)
+    with open(sum_file, "w") as fp:
+       json.dump(sums, fp, indent=4, separators=(",", ": "))
+
+
+def load_sums(json_dir, torrent_dir, sum_file):
+    with open(sum_file) as fp:
+        sums = json.load(fp)
+    make_str(sums)
+
+    sum_idx = {}
+    for path, sum in sums:
+        if path in sum_idx:
+            check(sum == sum_idx[path])
+            print("Warning: path {} in multiple torrents.".format(path))
+        sum_idx[path] = sum
+
+    torrents = load_torrents(json_dir)
+
+    for torrent_id, torrent in torrents.items():
+        if not "data" in torrent:
+            continue
+        info = torrent["data"]["info"]
+        download = torrent["download"]
+        md5s = download.setdefault("md5", [])
+        if not md5s:
+            for _ in get_torrent_files(info):
+                md5s.append("")
+
+        for i, (rel_path, file_length) in enumerate(get_torrent_files(info)):
+            src_path = os.path.join(torrent_dir, torrent_id, rel_path)
+            if os.path.islink(src_path):
+                src_path = os.readlink(src_path)
+            sum = sum_idx.pop(src_path, None)
+            if sum:
+                if md5s[i]:
+                    check(md5s[i] == sum)
+                else:
+                    md5s[i] = sum
+
+    print("Nonmatched", sum_idx)
+    save_torrents(torrents, json_dir)
+
+
+def make_symlink_tree(src_dir, dst_dir):
+    for src_root, dirs, files in os.walk(src_dir, topdown=False):
+        rel = os.path.relpath(src_root, src_dir)
+        dst_root = os.path.join(dst_dir, rel)
+        if not os.path.exists(dst_root): os.makedirs(dst_root)
+        for f in files:
+            src_file = os.path.join(src_root, f)
+            dst_file = os.path.join(dst_root, f)
+            subprocess.check_call(["ln", "-s", src_file, dst_file])
+            subprocess.check_call(["touch", "-hr", src_file, dst_file])
+        subprocess.check_call(["touch", "-r", src_root, dst_root])
+
+
+def save_torrents(torrents, json_dir):
+    make_unicode(torrents)
+    for torrent_id, t in torrents.iteritems():
+        save_json_torrent(t, json_dir, torrent_id)
+
+def load_torrents(json_dir):
+    torrents = {}
+    for torrent_id in list_json_torrents(json_dir):
+        torrents[torrent_id] = load_json_torrent(json_dir, torrent_id)
+    make_str(torrents)
+    return torrents
+
+
+def save_json_torrent(torrent, json_dir, torrent_id):
+    with open(os.path.join(json_dir, torrent_id + ".json"), "w") as fp:
+        json.dump(torrent, fp, indent=4, separators=(",", ": "))
+
+
+def load_json_torrent(json_dir, torrent_id):
+    with open(os.path.join(json_dir, torrent_id + ".json")) as fp:
+        return json.load(fp, object_pairs_hook=OrderedDict)
+
+
+def list_json_torrents(json_dir):
+    for f in os.listdir(json_dir):
+        if not f.endswith(".json"):
+            continue
+        yield  f[:-len(".json")]
+
+
+def get_torrent_files(info):
+    if "files" in info:
+        for file_info in info["files"]:
+            yield os.path.join(info["name"], *file_info["path"]), file_info["length"]
+    else:
+        yield info["name"], info["length"]
+
+
+def torrent_file(torrent, i):
+    info = torrent["data"]["info"]
+    if "files" in info:
+        return os.path.join(info["name"], info["files"][i]["path"])
+    check(i == 0, i)
+    return info["name"]
+
+
+def obj_dict(obj):
+    assert set(dir(obj)) - set(obj.__dict__.keys()) == {
+        "__doc__", "__init__", "__module__"}
+    return obj.__dict__.copy()
+
+
+def sort_dicts(obj):
+    return update_obj(obj, lambda v: OrderedDict(
+        x for x in sorted(v.items())) if isinstance(v, dict) else v)
+
+
+def make_unicode(obj):
+    return update_obj(obj,
+                      lambda v: decode_ascii_surrogateescape(v)
+                      if isinstance(v, str) else v)
+
+
+def make_str(obj):
+    return update_obj(obj,
+                      lambda v: encode_ascii_surrogateescape(v)
+                      if isinstance(v, unicode) else v)
+
+def update_obj(obj, cb):
+    obj = cb(obj)
+    if hasattr(obj, "items"):
+        for k, v in obj.items():
+            del obj[k]
+            obj[cb(k)] = update_obj(v, cb)
+    elif hasattr(obj, "__setitem__"):
+        for k, v in enumerate(obj):
+            obj[k] = update_obj(v, cb)
+    return obj
+
+
+def sort_keys(ordered_dict, keys):
+    update = []
+    for key in keys:
+        try:
+            update.append((key, ordered_dict[key]))
+        except KeyError:
+            pass
+        else:
+            del ordered_dict[key]
+    check(not ordered_dict)
+    ordered_dict.update(update)
+
+
+# Equivalent to python3: bytestr.decode("ascii", errors="surrogateescape"))
+def decode_ascii_surrogateescape(bytestr):
+    assert isinstance(bytestr, str)
+    return u"".join(unichr(b if b < 128 else b + 0xdc00)
+                    for b in map(ord, bytestr))
+
+
+# Equivalent to python3: unicodestr.encode("ascii", errors="surrogateescape"))
+def encode_ascii_surrogateescape(unicodestr):
+    assert isinstance(unicodestr, unicode)
+    return "".join(chr(c if c < 128 else c - 0xdc00)
+                   for c in map(ord, unicodestr))
+
+
+def field(obj, *path, **kwargs):
+    default_value = kwargs.pop("default_value", None)
+    assert not kwargs
+    for component in path:
+        if component not in obj:
+            return default_value
+        obj = obj[component]
+    return obj
+
+
+def check(cond, error=None):
+    if not cond:
+        raise Exception(error)
+
+
+def monkeypatch_bencode():
+    """Change deluge bencode decoder to preserve dict key order."""
+    def decode_dict(x, f):
+        r, f = OrderedDict(), f+1
+        while x[f] != "e":
+            k, f = bencode.decode_string(x, f)
+            r[k], f = bencode.decode_func[x[f]](x, f)
+        return (r, f + 1)
+
+    bencode.decode_dict = decode_dict
+    bencode.decode_func["d"] = decode_dict
+
+    def encode_ordered_dict(x,r):
+        r.append('d')
+        for k, v in x.iteritems():
+            r.extend((str(len(k)), ':', k))
+            bencode.encode_func[type(v)](v, r)
+        r.append('e')
+
+    bencode.encode_func[OrderedDict] = encode_ordered_dict
+
+
+monkeypatch_bencode()
