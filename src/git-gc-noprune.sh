47f6abfbabb63c4328f4a2b1ccbc2a7f780714c3
Russell Yanofsky <russ@yanofsky.org>
2016-04-02 20:53:11 -0400

    Disable repacking in src/git-gc-noprune.sh
    Made this change last week before git@20160325T014415Z backup.

--- src/git-gc-noprune.sh
+++ src/git-gc-noprune.sh
@@ -2,7 +2,8 @@
 # from http://stackoverflow.com/questions/12339165/how-to-pack-all-loose-objects-in-git
 set -e
 set -x
-git gc --no-prune
+# Repacking disabled by default, makes incremental backup more cumbersome.
+test -z "$REPACK" || git gc --no-prune
 git prune-packed
 find .git/objects/?? -type f | perl -pe 's@^\.git/objects/(..)/@$1@' | git pack-objects .git/objects/pack/pack
 git prune-packed
