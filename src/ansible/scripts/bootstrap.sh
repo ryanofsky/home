8aba68ee19c2d4c1d952c2507bb8dde4ea6fc4a1
Russell Yanofsky <russ@yanofsky.org>
2016-04-05 08:46:32 -0400

    ansible stage3 script, slightly less infuriating ansible ssh improvemnts (still extremely slow)

--- src/ansible/scripts/bootstrap.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-set -x
-
-cryptsetup --verbose --batch-mode luksFormat /dev/xvdb <<<"$LUKSPASS"
---
+++ src/ansible/scripts/mount-stage3.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+set -x
+set -e
+
+STAGE3=http://gentoo.mirrors.easynews.com/linux/gentoo/releases/amd64/autobuilds/current-stage3-amd64-hardened/stage3-amd64-hardened-20160331.tar.bz2
+ROOT=/dev/xvdc
+BOOT=/dev/xvda
+SWAP=/dev/xvdb
+
+# So prompts don't hang the script forever. Ansible is fucking stupid.
+exec < /dev/null
+
+# Cleanup resumed.
+umount -R /mnt/root || true
+rmdir /mnt/root || true
+cryptsetup close root || true
+swapoff /dev/mapper/swap || true
+cryptsetup close swap || true
+
+# Format filesystems
+yes | mke2fs "$BOOT"
+cryptsetup -d /dev/urandom create swap "$SWAP"
+mkswap /dev/mapper/swap
+swapon /dev/mapper/swap
+cryptsetup --verbose --batch-mode luksFormat "$ROOT" <<<"$LUKSPASS"
+cryptsetup --verbose --batch-mode open --type luks "$ROOT" root <<<"$LUKSPASS"
+mkfs.btrfs /dev/mapper/root
+
+# Mount root filesystem.
+mkdir /mnt/root
+mount -o noatime /dev/mapper/root /mnt/root
+btrfs su create /mnt/root/root
+btrfs su create /mnt/root/portage
+
+# Extract stage3
+cd /tmp
+test -e "${STAGE3##*/}" || wget "$STAGE3"
+tar xjpf "${STAGE3##*/}" -C /mnt/root/root
+
+# Create snapshot post-stage3.
+mkdir /mnt/root/.snapshots
+sdate() { date -u +"%Y%m%dT%H%M%SZ"; }
+btrfs su snapshot -r /mnt/root/root /mnt/root/.snapshots/root@$(sdate)
+
+# Mount other mounts.
+mkdir /mnt/root/root/usr/portage
+mount --bind /mnt/root/portage /mnt/root/root/usr/portage
+mount -o noatime "$BOOT" /mnt/root/root/boot
