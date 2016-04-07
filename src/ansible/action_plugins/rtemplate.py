6c84fc059944007887574f908b26d19a214e4013
Russell Yanofsky <russ@yanofsky.org>
2016-04-07 18:48:49 -0400

    add rtemplate module for tracking & replacing upstream configs

---
+++ src/ansible/action_plugins/rtemplate.py
@@ -0,0 +1,143 @@
+from ansible.plugins.action import ActionBase
+from ansible.utils.unicode import to_unicode
+from collections import OrderedDict
+import yaml
+import os
+import re
+import stat
+
+
+class TemplateFile:
+    def __init__(self):
+        self.template_path = None
+        self.info_path = None
+        self.upstream_paths = set()
+        self.temp_upstream_path = None
+
+
+class ActionModule(ActionBase):
+    def run(self, tmp=None, task_vars=None):
+        results = super(ActionModule, self).run(tmp, task_vars)
+        src = self._task.args.get("src", None)
+        dest = self._task.args.get("dest", None)
+        hostname = task_vars["inventory_hostname"]
+
+        # Group src files into TemplateFiles.
+        tfiles = {}  # template_path -> TemplateFile
+        for root, dirs, files in os.walk(src):
+            rel = os.path.relpath(root, src)
+            for filename in files:
+                path = os.path.join(rel, filename)
+                m = re.match(r"^(.*?)\.upstream(\..*)?$", path)
+                tfile = tfiles.setdefault(
+                    path if not m else m.group(1), TemplateFile())
+                if m:
+                    if m.group(2) == ".info":
+                        assert tfile.info_path is None
+                        tfile.info_path = path
+                    else:
+                        assert path not in tfile.upstream_paths
+                        tfile.upstream_paths.add(path)
+                else:
+                    assert tfile.template_path is None
+                    tfile.template_path = path
+
+        # Build pull request.
+        files = []
+        for path, tfile in tfiles.items():
+            if tfile.info_path is None:
+                tfile.info_path = path + ".upstream.info"
+                tfile.info = OrderedDict()
+            else:
+                with open(os.path.join(src, tfile.info_path)) as fp:
+                    tfile.info = ordered_load(fp, yaml.SafeLoader)
+
+            tfile.upstream_path = path + ".upstream." + hostname
+            if tfile.upstream_path not in tfile.upstream_paths:
+                tfile.upstream_path = path + ".upstream"
+
+            remote_path = os.path.join(dest, path)
+            pushed_prev = tfile.upstream_path in tfile.info
+            key = path
+            files.append((remote_path, pushed_prev, key))
+
+        pull_result = self._execute_module(module_name="rtemplate",
+                                           module_args=dict(mode="pull",
+                                                            files=files),
+                                           task_vars=task_vars)
+        if "exception" in pull_result:
+            raise Exception(pull_result["exception"])
+
+        for (key, content, st_mode, st_uid, st_gid, st_atime, st_mtime,
+             st_ctime, temp_upstream_path) in pull_result["pull_results"]:
+            tfile = tfiles[key]
+            tfile.temp_upstream_path = temp_upstream_path
+            with open(os.path.join(src, tfile.upstream_path), "wb") as fp:
+                fp.write(content)
+            info = tfile.info.setdefault(tfile.upstream_path, OrderedDict())
+            info["st_mode"] = st_mode
+            info["st_uid"] = st_uid
+            info["st_gid"] = st_gid
+            info["st_atime"] = st_atime
+            info["st_mtime"] = st_mtime
+            info["st_ctime"] = st_ctime
+            with open(os.path.join(src, tfile.info_path), "wb") as fp:
+                ordered_dump(tfile.info, fp, yaml.SafeDumper)
+            ordered_dump(info, Dumper=yaml.SafeDumper)
+
+        changes = []
+        for path, tfile in tfiles.items():
+            if tfile.template_path is not None:
+                remote_path = os.path.join(dest, path)
+                with open(os.path.join(src, tfile.template_path), 'r') as fp:
+                    content = self._templar.template(to_unicode(fp.read()))
+                symlink = tfile.upstream_path in tfile.info and stat.S_ISLNK(
+                    tfile.info[tfile.upstream_path][
+                        "st_mode"])
+                delete = False
+                changes.append((remote_path, content, symlink, delete))
+
+            if tfile.temp_upstream_path is not None:
+                remote_path = tfile.temp_upstream_path
+                content = None
+                symlink = None
+                delete = True
+                changes.append((remote_path, content, symlink, delete))
+
+        push_result = self._execute_module(module_name="rtemplate",
+                                           module_args=dict(mode="push",
+                                                            files=changes),
+                                           task_vars=task_vars)
+        if "exception" in push_result:
+            raise Exception(push_result["exception"])
+
+        results.update(pull_result=pull_result,
+                       push_result=push_result,
+                       changed=push_result["changed"])
+        return results
+
+
+# From http://stackoverflow.com/questions/5121931/in-python-how-can-you-load-yaml-mappings-as-ordereddicts
+def ordered_load(stream, Loader=yaml.Loader, object_pairs_hook=OrderedDict):
+    class OrderedLoader(Loader):
+        pass
+
+    def construct_mapping(loader, node):
+        loader.flatten_mapping(node)
+        return object_pairs_hook(loader.construct_pairs(node))
+
+    OrderedLoader.add_constructor(
+        yaml.resolver.BaseResolver.DEFAULT_MAPPING_TAG, construct_mapping)
+    return yaml.load(stream, OrderedLoader)
+
+
+def ordered_dump(data, stream=None, Dumper=yaml.Dumper, **kwds):
+    class OrderedDumper(Dumper):
+        pass
+
+    def _dict_representer(dumper, data):
+        return dumper.represent_mapping(
+            yaml.resolver.BaseResolver.DEFAULT_MAPPING_TAG, data.items())
+
+    OrderedDumper.add_representer(OrderedDict, _dict_representer)
+    return yaml.dump(data, stream, OrderedDumper, **kwds)
---
+++ src/ansible/library/rtemplate.py
@@ -0,0 +1,86 @@
+#!/usr/bin/python
+
+DOCUMENTATION = """
+---
+module: rtemplate
+short_description: Recursively push templates.
+description:
+     - The M(rtemplate) module, recursively updates templates on the host.
+options:
+  src:
+    description:
+      - Local template source directory
+    required: true
+  dest:
+    description:
+      - Remote destination directory.
+    required: true
+"""
+
+EXAMPLES = """
+# Call rtemplate
+- test_plugin: src=templates/etc dest=/etc
+"""
+
+
+def main():
+    module = AnsibleModule(argument_spec=dict(mode=dict(required=True,
+                                                        type="str"),
+                                              files=dict(required=True,
+                                                         type="list")))
+    mode = module.params["mode"]
+    files = module.params["files"]
+
+    ret = dict(changed=False)
+    if mode == "pull":
+        pull_results = ret["pull_results"] = []
+        for path, pushed_prev, key in files:
+            upstream_path = temp_upstream_path = None
+            if not pushed_prev and (os.path.exists(path) or
+                                    os.path.islink(path)):
+                upstream_path = path
+            else:
+                dirname, basename = os.path.split(path)
+                cfgs = sorted(glob.glob(os.path.join(
+                    glob_escape(dirname), "._cfg????_" + glob_escape(
+                        basename))))
+                if cfgs:
+                    upstream_path = temp_upstream_path = cfgs[0]
+
+            if upstream_path is not None:
+                s = os.lstat(upstream_path)
+                if stat.S_ISLNK(s.st_mode):
+                    content = os.readlink(upstream_path)
+                else:
+                    content = open(upstream_path).read()
+                pull_results.append(
+                    (key, content, s.st_mode, s.st_uid, s.st_gid, s.st_atime,
+                     s.st_mtime, s.st_ctime, temp_upstream_path))
+    elif mode == "push":
+        for path, content, symlink, delete in files:
+            if delete:
+                assert content is None
+                os.unlink(path)
+            elif symlink:
+                if os.path.exists(path) or os.path.islink(path):
+                    os.unlink(path)
+                os.symlink(content, path)
+            else:
+                with open(path, "wb") as fp:
+                    fp.write(content)
+        ret["changed"] = True
+
+    module.exit_json(**ret)
+
+
+def glob_escape(pathname):
+    # From python3 glob.escape.
+    return re.sub('([*?[])', r'[\1]', pathname)
+
+
+# this is magic, see lib/ansible/module_common.py
+from ansible.module_utils.basic import *
+import glob
+
+if __name__ == "__main__":
+    main()
