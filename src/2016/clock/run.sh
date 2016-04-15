e22a440440031177de1dbb6e7f5a85fb04e354c9
Russell Yanofsky <russ@yanofsky.org>
2016-04-15 15:55:36 -0400

    clockwork orange glossary script

---
+++ src/2016/clock/run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -x
+set -e
+
+rm -rvf "Anthony Burgess - A Clockwork Orange sub.epub" 0
+unzip -d 0 "Anthony Burgess - A Clockwork Orange.epub"
+python gloss.py
+cd 0
+zip -r ../idk.zip .
+cd ..
+rm -rf 0
+mv idk.zip "Anthony Burgess - A Clockwork Orange sub.epub"
+ebook-viewer "Anthony Burgess - A Clockwork Orange sub.epub" &
