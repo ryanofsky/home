abdce1edba69e71acb5640b0749caf61b5edfc0d
Russell Yanofsky <russ@yanofsky.org>
2016-08-06 21:17:56 -0400

    move bind-tools emerge to bootstrap script

--- src/ansible/scripts/script.sh
+++ src/ansible/scripts/script.sh
@@ -104,7 +104,7 @@ emerge-world() {
     emerge -qe --update --newuse --deep --with-bdeps=y @world
 
     # Add needed packages.
-    PKG="sys-kernel/hardened-sources app-editors/vim dev-util/cdiff sys-kernel/dracut sys-fs/btrfs-progs app-portage/gentoolkit app-portage/eix app-portage/genlop sys-apps/gptfdisk app-misc/screen app-portage/cpuid2cpuflags"
+    PKG="sys-kernel/hardened-sources app-editors/vim dev-util/cdiff sys-kernel/dracut sys-fs/btrfs-progs app-portage/gentoolkit app-portage/eix app-portage/genlop sys-apps/gptfdisk app-misc/screen net-dns/bind-tools app-portage/cpuid2cpuflags"
     if [ -n "$BOOT_DEV" ]; then
         PKG="$PKG sys-boot/grub"
     fi
