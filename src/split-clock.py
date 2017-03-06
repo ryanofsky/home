41ec0cec457cbb4a968b9e4af6752233371c7e09
Russell Yanofsky <russ@yanofsky.org>
2017-03-06 10:39:22 -0500

    add split-clock option for choosing date

--- src/split-clock.py
+++ src/split-clock.py
@@ -5,11 +5,12 @@ import re
 
 l = sys.stdin.readline()
 
-m = re.compile("(.*?)(\\[[0-9]{4}-[0-9]{2}-[0-9]{2} [A-Z][a-z]{2} [0-9]{2}:[0-9]{2})(]--)(\\[[0-9]{4}-[0-9]{2}-[0-9]{2} [A-Z][a-z]{2} )([0-9]{2}:[0-9]{2})(].*)", re.DOTALL).match(l)
+m = re.compile("(.*?)((\\[[0-9]{4}-[0-9]{2}-[0-9]{2} [A-Z][a-z]{2} )[0-9]{2}:[0-9]{2})(]--)(\\[[0-9]{4}-[0-9]{2}-[0-9]{2} [A-Z][a-z]{2} )([0-9]{2}:[0-9]{2})(].*)", re.DOTALL).match(l)
 if m:
-  prefix, first, sep, date, time, suffix = m.groups()
+  prefix, first, date1, sep, date2, time, suffix = m.groups()
   stime = sys.argv[1] if len(sys.argv) > 1 else time
-  sys.stdout.write(prefix + first + sep + date + stime + suffix + prefix + date + stime + sep + date + time + suffix)
+  date = date1 if len(sys.argv) > 2 else date2
+  sys.stdout.write(prefix + first + sep + date + stime + suffix + prefix + date + stime + sep + date2 + time + suffix)
 else:
   sys.stdout.write(l)
 sys.stdout.write(sys.stdin.read())
