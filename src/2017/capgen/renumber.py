948bfa7a09a13c80f5c28f71c61268fe94775ede
Russell Yanofsky <russ@yanofsky.org>
2017-04-12 06:04:27 -0400

    capnp field renumber script

---
+++ src/2017/capgen/renumber.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+import sys
+import re
+
+next = 0 if len(sys.argv) < 2 else int(sys.argv[1])
+
+def f(m):
+  global next
+  next += 1
+  return "@" + str(next - 1)
+
+sys.stdout.write(re.sub(r"@\d+", f, sys.stdin.read()))
