1f4c11dad14d3744782f38c63a3f6929342374d8
Russell Yanofsky <russ@yanofsky.org>
2017-06-23 23:11:43 -0400

    pom.sh: get rid of uninteruptable sleep

--- src/pom.sh
+++ src/pom.sh
@@ -10,31 +10,28 @@ popup() {
 if [ "$1" = monitor ]; then
     mkfifo "$FIFO"
     exec 3<>"$FIFO"
-    TIMEOUTS=0
+    TIMEOUTS=0 # number of timeouts
+    DUR=$TIMEOUT
     while true; do
-        if read -u 3 -t "$TIMEOUT" T; then
-            echo "$(date) -- sleep $T"
-            sleep $T
-            echo "$(date) -- popup"
-            popup
+        if read -u 3 -t "$DUR" T; then
+            DUR=$(sed s/m/*60/ <<<"$T" | bc)
+            echo "$(date) -- sleep $T ($DUR)"
             TIMEOUTS=0
             continue
         fi
 
         if screenlock.sh check; then
             TIMEOUTS=0
-            continue
-        fi
-
-        ((++TIMEOUTS))
-
-        if [ "$TIMEOUTS" -lt 5 ]; then
+        elif [ "$TIMEOUTS" -lt 4 ]; then
             echo "$(date) -- popup ($TIMEOUTS timeouts)"
             popup
         else
             echo "$(date) -- lock ($TIMEOUTS timeouts)"
             screenlock.sh lock
         fi
+
+        DUR=$TIMEOUT
+        ((++TIMEOUTS))
     done
 else
     T=${1:-25m}
