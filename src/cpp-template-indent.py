9087c655e6e5667ec9dd424bce6bff7243e7645b
Russell Yanofsky <russ@yanofsky.org>
2017-09-12 05:58:19 -0400

    cpp-template-indent: hack to work with clang (2017-09-11 16:31:21.893324701 -0400)

--- src/cpp-template-indent.py
+++ src/cpp-template-indent.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python
+# coding=utf8
 
 import sys
 
 def indent(n):
   n = n.replace("basic_string<char, std::char_traits<char>, std::allocator<char> >", "string")
   n = n.replace("basic_string<char, char_traits<char>, allocator<char> >", "string")
+  n = n.replace("std::__cxx11::basic_string<char>", "string")
+  n = n.replace("std::__cxx11::basic_string<char, std::char_traits<char>, std::allocator<char> >", "string")
 
-  print "(" * 5, n, ")" * 5
+  #print "(" * 5, n, ")" * 5
 
   o = ''
   indent = 0
@@ -14,7 +17,7 @@ def indent(n):
   counts = []
   column = 0
   for c in n:
-    if c in ">)":
+    if c in ">)]":
       if not counts:
         return n
       o += "{}{} {} ".format(" " * max(1, 60 - column), "#" * len(counts), counts[-1])
@@ -33,14 +36,18 @@ def indent(n):
 
     newline = False
 
-    if c in "<(":
+    if c in "<([":
       counts.append(0)
       indent += 2
       newline = True
     elif c == ",":
-      o += "{}{} {} ".format(" " * max(1, 60 - column), "#" * len(counts), counts[-1])
-      counts[-1] += 1
+      o += "{}{} {} ".format(" " * max(1, 60 - column), "#" * len(counts), counts[-1] if counts else "BadCount")
+      if counts:
+        counts[-1] += 1
       newline = True
   return o
 
-sys.stdout.write("\n'".join(map(indent, sys.stdin.read().split("'"))))
+#sys.stdout.write("\n'".join(map(indent, sys.stdin.read().split("'"))))
+
+import re
+sys.stdout.write(re.sub("(‘)(.*?)(’)", lambda m: m.group(1) + indent(m.group(2)) + m.group(3), sys.stdin.read()))
