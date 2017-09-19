edfd6188ee1cd8e9a33dbd4d023e89237b4b48d7
Russell Yanofsky <russ@yanofsky.org>
2017-09-19 15:21:55 -0400

    chrome-remote-desktop-start.sh: fix screen size

--- src/chrome-remote-desktop-start.sh
+++ src/chrome-remote-desktop-start.sh
@@ -6,4 +6,4 @@ if [ ! -f $CHROME_REMOTE_DESKTOP_LOG_FILE ]; then
   sudo chmod 0600  $CHROME_REMOTE_DESKTOP_LOG_FILE
   sudo chown $USER $CHROME_REMOTE_DESKTOP_LOG_FILE
 fi
-exec /opt/google/chrome-remote-desktop/chrome-remote-desktop -s 1200x600 -f --start
+exec /opt/google/chrome-remote-desktop/chrome-remote-desktop -f --start
