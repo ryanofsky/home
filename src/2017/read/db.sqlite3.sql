7f19f60cf551676c195175b8252dea85c00664ec
Russell Yanofsky <russ@yanofsky.org>
2017-10-22 07:55:06 -0400

    echo .dump | sqlite3 db.sqlite3 > db.sqlite3.sql

---
+++ src/2017/read/db.sqlite3.sql
@@ -0,0 +1,5 @@
+PRAGMA foreign_keys=OFF;
+BEGIN TRANSACTION;
+CREATE TABLE IF NOT EXISTS "django_migrations" ("id" integer NOT NULL PRIMARY KEY AUTOINCREMENT, "app" varchar(255) NOT NULL, "name" varchar(255) NOT NULL, "applied" datetime NOT NULL);
+DELETE FROM sqlite_sequence;
+COMMIT;
