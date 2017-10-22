ff0c32f93455bf137e5c1e54239740cb774a02d4
Russell Yanofsky <russ@yanofsky.org>
2017-10-22 08:08:53 -0400

    python manage.py makemigrations combine

--- src/2017/read/combine/models.py
+++ src/2017/read/combine/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Url(models.Model):
+    source = models.CharField(max_length=20)
+    url = models.CharField(max_length=1000)
+    title = models.CharField(max_length=1000)
+    site = models.CharField(max_length=1000)
+    score = models.IntegerField()
+    comments = models.IntegerField()
+
+class Event(models.Model):
+    url = models.ForeignKey(Url, on_delete=models.PROTECT)
+    date = models.DateTimeField()
+    rating = models.IntegerField()
