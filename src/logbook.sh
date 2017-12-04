de6925a40c5e790cff344fd0ce0a4fc1c728f6ab
Russell Yanofsky <russ@yanofsky.org>
2017-12-04 14:31:05 -0500

    fix logbook.sh spacing

--- src/logbook.sh
+++ src/logbook.sh
@@ -3,7 +3,7 @@
 prev=
 while read weekday month day time tz year rest; do
     date=$(date --date="$weekday $month $day $time $tz $year" '+%Y-%m-%d %a %H:%M')
-    echo "CLOCK: [${prev:-$date}]--[$date] => 0:00"
+    echo "CLOCK: [${prev:-$date}]--[$date] =>  0:00"
     echo "- $rest"
     prev="$date"
 done < ~/.ln/org/date
