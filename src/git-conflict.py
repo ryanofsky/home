b4acd00d614c98ec41b0d9a22d2696740403fdeb
Russell Yanofsky <russ@yanofsky.org>
2018-03-14 04:56:05 -0400

    add git-conflict.py, conflict marker cleanup script

---
+++ src/git-conflict.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+import sys
+import re
+
+_r = re.compile(r"^<<<<<<<[^\n]*\n(.*?)\|\|\|\|\|\|\|[^\n]*\n(.*?)=======[^\n]*\n(.*?)>>>>>>>[^\n]*\n", re.DOTALL | re.M)
+
+def sub(m):
+    d1, d2, d3 = m.groups()
+    if d1 == d2:
+        return d3
+    elif d2 == d3:
+        return d1
+    return m.group(0)
+
+for f in sys.argv[1:]:
+    with open(f) as fp:
+        s = fp.read()
+    with open(f, "w") as fp:
+        fp.write(_r.sub(sub, s))
