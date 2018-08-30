c86d5afeb8fb41379b5b34cbda308bd42a5df693
Russell Yanofsky <russ@yanofsky.org>
2018-08-30 06:13:18 -0400

    logbook.sh: compute intervals

--- src/logbook.sh
+++ src/logbook.sh
@@ -3,9 +3,17 @@
 prev=
 while read weekday month day time tz year rest; do
     date=$(date --date="$weekday $month $day $time $tz $year" '+%Y-%m-%d %a %H:%M')
-    test "$prev" = "$date" || echo "CLOCK: [${prev:-$date}]--[$date] =>  0:00"
+    dsec=$(date --date="$date" '+%s')
+    sec=$((dsec-psec))
+    min=$((sec/60))
+    sec=$((sec%60))
+    hour=$((min/60))
+    min=$((min%60))
+    dur=$(printf "%2d:%02d" "$hour" "$min")
+    test "$prev" = "$date" || echo "CLOCK: [${prev:-$date}]--[$date] => $dur"
     echo "- $rest"
     prev="$date"
+    psec="$dsec"
 done < ~/work/logbook
 
 if [ "$1" = 1 ]; then
