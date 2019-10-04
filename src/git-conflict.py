027bc886509ffcef22f0f4203e95a6f4c00d77b5
Russell Yanofsky <russ@yanofsky.org>
2019-10-04 13:14:07 -0400

    git-conflict.py: write ours/base/theirs d1/d2/d3 files

--- src/git-conflict.py
+++ src/git-conflict.py
@@ -2,6 +2,7 @@
 
 import sys
 import re
+import pathlib
 
 _r = re.compile(r"^<<<<<<<[^\n]*\n(.*?)\|\|\|\|\|\|\|[^\n]*\n(.*?)=======[^\n]*\n(.*?)>>>>>>>[^\n]*\n", re.DOTALL | re.M)
 
@@ -11,7 +12,19 @@ def sub(m):
         return d3
     elif d2 == d3 or d1 == d3:
         return d1
+    conflict(d1, d2, d3)
     return m.group(0)
+
+def conflict(d1, d2, d3):
+    global fp1, fp2, fp3, f
+    for fp, d in ((fp1, d1), (fp2, d2), (fp3, d3)):
+        with fp.open("at") as out:
+            out.write(f":::::: {f} ::::::\n{d}\n")
+
+fp1 = pathlib.Path("d1")
+fp2 = pathlib.Path("d2")
+fp3 = pathlib.Path("d3")
+if fp1.is_file() and fp2.is_file() and fp3.is_file(): fp1.unlink(), fp2.unlink(), fp3.unlink()
 
 for f in sys.argv[1:]:
     with open(f) as fp:
