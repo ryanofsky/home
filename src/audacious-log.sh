2611441cbd5ce15ab97cc870f04945e7bc0497d0
Russell Yanofsky <russ@yanofsky.org>
2021-02-11 05:44:47 -0500

    audacious location

--- src/audacious-log.sh
+++ src/audacious-log.sh
@@ -23,4 +23,4 @@ for arg in "$@"; do
     fi
     s="$s $(printf "%q" "$arg")"
 done
-echo "$s" >> "$HOME/.ln/org/log/$(date +"%Y-%m-%d")-audacious.log"
+echo "$s" >> "$HOME/.ln/audacious/$(date +"%Y-%m-%d").log"
